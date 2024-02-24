7fe2e6f1a7b7a0b1436aeeae89d83c54e0a0dff7
YarTom <yartomilin@mail.ru>
2024-02-24 15:03:48 +0300

    ver2

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float MinAngle;
+    public float MaxAngle;
+    public Transform CameraAxisTransform;
+    public float RotateSpeed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newAngleY = transform.localEulerAngles.y + Time.deltaTime * RotateSpeed * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+
+
+
+        var NewAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotateSpeed * Input.GetAxis("Mouse Y");
+        
+
+        if (NewAngleX > 180)
+            NewAngleX -= 360;
+        NewAngleX = Mathf.Clamp(NewAngleX, MaxAngle, MinAngle);
+
+
+
+        CameraAxisTransform.localEulerAngles = new Vector3(NewAngleX, 0, 0);
+
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _fallVelocity = 0;
+
+    private CharacterController _charactercontroller;
+    private Animator _animator;
+
+    private Vector3 _movevector;
+
+    public float Gravity;
+    public float JumpForce;
+    public float Speed;
+
+    private void Start()
+    {
+        _charactercontroller = GetComponent<CharacterController>();
+
+        _animator = GetComponent<Animator>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        ///Movement
+        _charactercontroller.Move(_movevector * Speed * Time.fixedDeltaTime);
+
+        ///Fall&Jump
+        _fallVelocity += Gravity * Time.fixedDeltaTime;
+        _charactercontroller.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+        if (_charactercontroller.isGrounded) _fallVelocity = 0;
+    }
+
+    private void Update()
+    {
+        _movevector = Vector3.zero;
+
+        ///Jump
+        if (Input.GetKeyDown(KeyCode.Space) && _charactercontroller.isGrounded)
+        {
+            _fallVelocity = -JumpForce;
+        }
+
+        ///Movement
+        if (Input.GetKey(KeyCode.W)) _movevector += transform.forward;
+        if (Input.GetKey(KeyCode.A)) _movevector -= transform.right;
+        if (Input.GetKey(KeyCode.S)) _movevector -= transform.forward;
+        if (Input.GetKey(KeyCode.D)) _movevector += transform.right;
+        
+        if(_movevector == Vector3.zero)
+        {
+            _animator.SetBool("IsRunning", false);
+        }
+        else
+        {
+            _animator.SetBool("IsRunning", true);
+        }
+    }
+}
